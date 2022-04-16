a514d3eba81c9185d6255ac2ad2dbeef3a151df8
kotleni <yavarenikya@gmail.com>
2022-04-16 22:37:54 +0300

    initial

--- WhiteNoise/ViewController.swift
+++ WhiteNoise/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let mainView = MainView()
+    
     override func viewDidLoad() {
+        view = mainView
+        view.backgroundColor = .systemBackground // set background
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainView.viewDidLoad()
     }
-
 
 }
 
+class MainView: UIView {
+    private let textView = UITextView()
+    
+    func viewDidLoad() {
+        // add textview
+        addSubview(textView)
+        
+        // setup textview
+        textView.text = "WhiteNoise"
+        textView.textColor = .black
+        
+        // setup contraint for textview
+        NSLayoutConstraint.activate([
+            textView.topAnchor.constraint(equalTo: self.topAnchor),
+            textView.leftAnchor.constraint(equalTo: self.leftAnchor),
+            textView.widthAnchor.constraint(equalToConstant: 200),
+            textView.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    }
+}
