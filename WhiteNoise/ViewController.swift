8119045384ef5bbe0a92ca1ffe1e4ce971b7cd10
kotleni <yavarenikya@gmail.com>
2022-04-16 22:17:06 +0300

    Initial Commit

---
+++ WhiteNoise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhiteNoise
+//
+//  Created by Victor Varenik on 16.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
