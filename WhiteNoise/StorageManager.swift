6302af87f3d4c57538214eafb25ca593c0b1badc
kotleni <yavarenikya@gmail.com>
2022-04-17 13:53:59 +0300

    nometa

--- WhiteNoise/StorageManager.swift
+++ WhiteNoise/StorageManager.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 class StorageManager {
+    enum StorageKey: String {
+        case SLEEPTIME = "sleep_hour"
+        case WAKEUPTIME = "sleep_minute"
+    }
+    
     static var shared: StorageManager = {
         let manager = StorageManager()
         
@@ -15,4 +20,20 @@ class StorageManager {
     }()
     
     private let defaults = UserDefaults.standard
+    
+    // get sleep time
+    func getSleepTime() -> Time? {
+        return defaults.object(forKey: StorageKey.SLEEPTIME.rawValue) as! Time?
+    }
+    
+    // get wakeup time
+    func getWakeUpTime() -> Time? {
+        return defaults.object(forKey: StorageKey.WAKEUPTIME.rawValue) as! Time?
+    }
+    
+    // set sleep time
+    func setTimes(sleep: Time, wakeup: Time) {
+        defaults.set(sleep, forKey: StorageKey.SLEEPTIME.rawValue)
+        defaults.set(wakeup, forKey: StorageKey.WAKEUPTIME.rawValue)
+    }
 }
---
+++ WhiteNoise/Time.swift
@@ -0,0 +1,13 @@
+//
+//  Time.swift
+//  WhiteNoise
+//
+//  Created by Victor Varenik on 17.04.2022.
+//
+
+import Foundation
+
+struct Time: Codable {
+    let hour: Int
+    let minute: Int
+}
