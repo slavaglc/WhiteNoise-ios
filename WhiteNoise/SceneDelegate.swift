f9d96387ec5898a80597ded9a787d35ce42bca7b
kotleni <yavarenikya@gmail.com>
2022-04-18 15:02:02 +0300

    fix

--- WhiteNoise/SceneDelegate.swift
+++ WhiteNoise/SceneDelegate.swift
@@ -17,20 +17,31 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(windowScene: scene)
         window?.rootViewController = MyNavigationController(rootViewController: PlansleepViewController())
         window?.makeKeyAndVisible()
+        
+        StorageManager.shared.increaseRunsCount()
+        
+        #if DEBUG
+        print("Running debug build.")
+        #endif
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
+        
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
+        
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
+        
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
+        
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
+        
     }
 }
