bae8cb7c907d5e34082b88fb41a6f27348aeb697
slavaglc <47712523+slavaglc@users.noreply.github.com>
2022-04-21 18:02:13 +0300

    added FiltertTag model

---
+++ WhiteNoise/Models/FilterTag.swift
@@ -0,0 +1,23 @@
+//
+//  FilterTag.swift
+//  WhiteNoise
+//
+//  Created by Вячеслав Макаров on 21.04.2022.
+//
+
+import Foundation
+
+
+struct FilterTag {
+    let title: String
+    
+    static func getAllFilterTags() -> [FilterTag] {
+        [
+            FilterTag(title: "All"),
+            FilterTag(title: "Weather"),
+            FilterTag(title: "Animals"),
+            FilterTag(title: "Vehicle")
+            
+        ]
+    }
+}
